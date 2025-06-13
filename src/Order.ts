5814c33c3270ac0f1988892f2807ca37577c55f1
ernie <lawwingyeung@gmail.com>
2025-06-13 14:27:38 -0400

    worked more on the culminating

--- src/Order.ts
+++ src/Order.ts
@@ -1,4 +1,22 @@
 export class Order {
     private orderID : number;
-    private customer: Customer
+    private totalAmount: number;
+    private _orderStatus : string;
+
+
+    public updateStatus() : string {
+
+    };
+
+    public assignShipping() : string {
+
+    };
+
+    protected get orderStatus() : string{
+        return ''
+    }
+
+    private checkInventory(productName : string) : string{
+        
+    }
 }
