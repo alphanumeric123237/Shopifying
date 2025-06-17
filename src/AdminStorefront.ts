504d79a90e0c13e456b3391e2e8de3f93785dca2
colin <yim.colin@gmail.com>
2025-06-17 15:18:42 -0400

    added a get method to updateCustomer

--- src/AdminStorefront.ts
+++ src/AdminStorefront.ts
@@ -30,7 +30,38 @@ class Customer {
       verified_email: this.verifiedEmail,
     };
   }
-  
+}
+
+class UpdatedCustomerInfo {
+  private firstName ?: string;
+  private lastName ?: string
+  private email ?: string;
+  private phoneNumber ?: string;
+  private verifiedEmail ?: boolean;
+
+  private updatedCustomer: {
+    first_name?: string;
+    last_name?: string,
+    email?: string,
+    phone?: string,
+    verified_email?: boolean
+  }
+
+  public constructor(first ?: string, last ?: string, email ?: string, phone ?: string, verification ?: boolean) {
+    this.firstName = first;
+    this.lastName  = last;
+    this.email = email;
+    this.phoneNumber = phone;
+    this.verifiedEmail = verification;
+
+    this.updatedCustomer = {
+      first_name: this.firstName,
+      last_name: this.lastName,
+      email: this.email,
+      phone: this.phoneNumber,
+      verified_email: this.verifiedEmail,
+    };
+  }
 }
 
 class AdminStoreFront {
@@ -75,7 +106,7 @@ class AdminStoreFront {
 
   protected async addCustomer(customerData: Customer) : Promise<void> {
 
-    const requestOptions: RequestInit = {
+    const REQUEST_OPTIONS: RequestInit = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -85,19 +116,34 @@ class AdminStoreFront {
     };
 
     try {
-      const response = await fetch(this.url, requestOptions);
-      const responseBody = await response.json();
+      const RESPONSE = await fetch(this.url, REQUEST_OPTIONS);
+      const RESPONDE_BODY = await RESPONSE.json();
 
-      if (!response.ok) {
-        console.error("Shopify API error response:", responseBody);
-        throw new Error(`Shopify API error: ${responseBody.errors || JSON.stringify(responseBody)}`);
+      if (!RESPONSE.ok) {
+        console.error("Shopify API error response:", RESPONDE_BODY);
+        throw new Error(`Shopify API error: ${RESPONDE_BODY.errors || JSON.stringify(RESPONDE_BODY)}`);
       }
 
-      console.log("Customer created:", responseBody.customer);
+      console.log("Customer created:", RESPONDE_BODY.customer);
     } catch (error) {
       console.error("Error creating customer:", error);
     }
   }  
-  private updateCustomer(id: string) : void {
-  }
+  private async updateCustomer(email : string, updatedCustomerInfo : UpdatedCustomerInfo) : Promise<void> {
+    //searches for a customer with given email, when found, update them with given customer data 
+    const SEARCH_QUERY = await fetch (`https://stringliteral.myshopify.com/admin/api/2025-04/customers/search.json?query=email:${encodeURIComponent(email)}`,
+    {
+      method : "GET",
+      headers : {
+        "Content-Type": "application/json"
+      }
+    });
+    const SEARCH = await SEARCH_QUERY.json();
+    //check if response is valid
+    if (!SEARCH.customers || SEARCH.)
+
+
+
+  }//end of function
 }
+
